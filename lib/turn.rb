7d565fc4a3e62c268fda49655ee567e1e4e44b85
Casey Zimmer <czimmer@ramapo.edu>
2018-08-27 01:52:00 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,3 +17,11 @@ def position_taken?(board, index)
   return true
   end
 end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end

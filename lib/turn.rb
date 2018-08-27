fd9c0b38f9c0cc998e49618c2502e93228e0f7c1
Casey Zimmer <czimmer@ramapo.edu>
2018-08-27 01:51:22 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,1 +1,19 @@
 
+def valid_move?(board, index)
+  
+  if(position_taken?(board,index))
+    return false;
+  elsif(index < board.length)
+    return true;
+  end
+  
+end
+
+
+def position_taken?(board, index)
+  if(board[index] === " " || board[index] ==="" || board[index] === nil)
+    return false
+  elsif(board[index] ==="X" || board[index] ==="O")
+  return true
+  end
+end
